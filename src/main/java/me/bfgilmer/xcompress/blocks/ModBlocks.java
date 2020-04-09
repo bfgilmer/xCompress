2a72d2ddcf3c5400d6ed631926697401ccdc0429
bgilmer <bgilmer@742DHR2.americas.cray.com>
2020-04-08 19:11:22 -0500

    Completed changes to xCompress

--- src/main/java/me/bfgilmer/xcompress/blocks/ModBlocks.java
+++ src/main/java/me/bfgilmer/xcompress/blocks/ModBlocks.java
@@ -7,349 +7,349 @@ import net.minecraftforge.registries.ObjectHolder;
 public class ModBlocks {
 
 
-    @ObjectHolder("compressium:soulsand_1")
+    @ObjectHolder("xcompress:soulsand_1")
     public static Block SOULSAND_1;
 
-    @ObjectHolder("compressium:soulsand_2")
+    @ObjectHolder("xcompress:soulsand_2")
     public static Block SOULSAND_2;
 
-    @ObjectHolder("compressium:soulsand_3")
+    @ObjectHolder("xcompress:soulsand_3")
     public static Block SOULSAND_3;
 
-    @ObjectHolder("compressium:soulsand_4")
+    @ObjectHolder("xcompress:soulsand_4")
     public static Block SOULSAND_4;
 
-    @ObjectHolder("compressium:soulsand_5")
+    @ObjectHolder("xcompress:soulsand_5")
     public static Block SOULSAND_5;
 
-    @ObjectHolder("compressium:soulsand_6")
+    @ObjectHolder("xcompress:soulsand_6")
     public static Block SOULSAND_6;
 
-    @ObjectHolder("compressium:soulsand_7")
+    @ObjectHolder("xcompress:soulsand_7")
     public static Block SOULSAND_7;
 
-    @ObjectHolder("compressium:soulsand_8")
+    @ObjectHolder("xcompress:soulsand_8")
     public static Block SOULSAND_8;
 
-    @ObjectHolder("compressium:soulsand_9")
+    @ObjectHolder("xcompress:soulsand_9")
     public static Block SOULSAND_9;
 
 
-    @ObjectHolder("compressium:snow_1")
+    @ObjectHolder("xcompress:snow_1")
     public static Block SNOW_1;
 
-    @ObjectHolder("compressium:snow_2")
+    @ObjectHolder("xcompress:snow_2")
     public static Block SNOW_2;
 
-    @ObjectHolder("compressium:snow_3")
+    @ObjectHolder("xcompress:snow_3")
     public static Block SNOW_3;
 
-    @ObjectHolder("compressium:snow_4")
+    @ObjectHolder("xcompress:snow_4")
     public static Block SNOW_4;
 
-    @ObjectHolder("compressium:snow_5")
+    @ObjectHolder("xcompress:snow_5")
     public static Block SNOW_5;
 
-    @ObjectHolder("compressium:snow_6")
+    @ObjectHolder("xcompress:snow_6")
     public static Block SNOW_6;
 
-    @ObjectHolder("compressium:snow_7")
+    @ObjectHolder("xcompress:snow_7")
     public static Block SNOW_7;
 
-    @ObjectHolder("compressium:snow_8")
+    @ObjectHolder("xcompress:snow_8")
     public static Block SNOW_8;
 
-    @ObjectHolder("compressium:snow_9")
+    @ObjectHolder("xcompress:snow_9")
     public static Block SNOW_9;
 
 
 
 
-    @ObjectHolder("compressium:netherrack_1")
+    @ObjectHolder("xcompress:netherrack_1")
     public static Block NETHERRACK_1;
 
-    @ObjectHolder("compressium:netherrack_2")
+    @ObjectHolder("xcompress:netherrack_2")
     public static Block NETHERRACK_2;
 
-    @ObjectHolder("compressium:netherrack_3")
+    @ObjectHolder("xcompress:netherrack_3")
     public static Block NETHERRACK_3;
 
-    @ObjectHolder("compressium:netherrack_4")
+    @ObjectHolder("xcompress:netherrack_4")
     public static Block NETHERRACK_4;
 
-    @ObjectHolder("compressium:netherrack_5")
+    @ObjectHolder("xcompress:netherrack_5")
     public static Block NETHERRACK_5;
 
-    @ObjectHolder("compressium:netherrack_6")
+    @ObjectHolder("xcompress:netherrack_6")
     public static Block NETHERRACK_6;
 
-    @ObjectHolder("compressium:netherrack_7")
+    @ObjectHolder("xcompress:netherrack_7")
     public static Block NETHERRACK_7;
 
-    @ObjectHolder("compressium:netherrack_8")
+    @ObjectHolder("xcompress:netherrack_8")
     public static Block NETHERRACK_8;
 
-    @ObjectHolder("compressium:netherrack_9")
+    @ObjectHolder("xcompress:netherrack_9")
     public static Block NETHERRACK_9;
 
 
     // Clay
-    @ObjectHolder("compressium:clay_1")
+    @ObjectHolder("xcompress:clay_1")
     public static Block CLAY_1;
 
-    @ObjectHolder("compressium:clay_2")
+    @ObjectHolder("xcompress:clay_2")
     public static Block CLAY_2;
 
-    @ObjectHolder("compressium:clay_3")
+    @ObjectHolder("xcompress:clay_3")
     public static Block CLAY_3;
 
-    @ObjectHolder("compressium:clay_4")
+    @ObjectHolder("xcompress:clay_4")
     public static Block CLAY_4;
 
-    @ObjectHolder("compressium:clay_5")
+    @ObjectHolder("xcompress:clay_5")
     public static Block CLAY_5;
 
-    @ObjectHolder("compressium:clay_6")
+    @ObjectHolder("xcompress:clay_6")
     public static Block CLAY_6;
 
-    @ObjectHolder("compressium:clay_7")
+    @ObjectHolder("xcompress:clay_7")
     public static Block CLAY_7;
 
-    @ObjectHolder("compressium:clay_8")
+    @ObjectHolder("xcompress:clay_8")
     public static Block CLAY_8;
 
-    @ObjectHolder("compressium:clay_9")
+    @ObjectHolder("xcompress:clay_9")
     public static Block CLAY_9;
 
     // Cobblestone
-    @ObjectHolder("compressium:cobblestone_1")
+    @ObjectHolder("xcompress:cobblestone_1")
     public static Block COBBLESTONE_1;
 
-    @ObjectHolder("compressium:cobblestone_2")
+    @ObjectHolder("xcompress:cobblestone_2")
     public static Block COBBLESTONE_2;
 
-    @ObjectHolder("compressium:cobblestone_3")
+    @ObjectHolder("xcompress:cobblestone_3")
     public static Block COBBLESTONE_3;
 
-    @ObjectHolder("compressium:cobblestone_4")
+    @ObjectHolder("xcompress:cobblestone_4")
     public static Block COBBLESTONE_4;
 
-    @ObjectHolder("compressium:cobblestone_5")
+    @ObjectHolder("xcompress:cobblestone_5")
     public static Block COBBLESTONE_5;
 
-    @ObjectHolder("compressium:cobblestone_6")
+    @ObjectHolder("xcompress:cobblestone_6")
     public static Block COBBLESTONE_6;
 
-    @ObjectHolder("compressium:cobblestone_7")
+    @ObjectHolder("xcompress:cobblestone_7")
     public static Block COBBLESTONE_7;
 
-    @ObjectHolder("compressium:cobblestone_8")
+    @ObjectHolder("xcompress:cobblestone_8")
     public static Block COBBLESTONE_8;
 
-    @ObjectHolder("compressium:cobblestone_9")
+    @ObjectHolder("xcompress:cobblestone_9")
     public static Block COBBLESTONE_9;
 
 // Sand
 
-    @ObjectHolder("compressium:sand_1")
+    @ObjectHolder("xcompress:sand_1")
     public static Block SAND_1;
 
-    @ObjectHolder("compressium:sand_2")
+    @ObjectHolder("xcompress:sand_2")
     public static Block SAND_2;
 
-    @ObjectHolder("compressium:sand_3")
+    @ObjectHolder("xcompress:sand_3")
     public static Block SAND_3;
 
-    @ObjectHolder("compressium:sand_4")
+    @ObjectHolder("xcompress:sand_4")
     public static Block SAND_4;
 
-    @ObjectHolder("compressium:sand_5")
+    @ObjectHolder("xcompress:sand_5")
     public static Block SAND_5;
 
-    @ObjectHolder("compressium:sand_6")
+    @ObjectHolder("xcompress:sand_6")
     public static Block SAND_6;
 
-    @ObjectHolder("compressium:sand_7")
+    @ObjectHolder("xcompress:sand_7")
     public static Block SAND_7;
 
-    @ObjectHolder("compressium:sand_8")
+    @ObjectHolder("xcompress:sand_8")
     public static Block SAND_8;
 
-    @ObjectHolder("compressium:sand_9")
+    @ObjectHolder("xcompress:sand_9")
     public static Block SAND_9;
 
 // Gravel
 
-    @ObjectHolder("compressium:gravel_1")
+    @ObjectHolder("xcompress:gravel_1")
     public static Block GRAVEL_1;
 
-    @ObjectHolder("compressium:gravel_2")
+    @ObjectHolder("xcompress:gravel_2")
     public static Block GRAVEL_2;
 
-    @ObjectHolder("compressium:gravel_3")
+    @ObjectHolder("xcompress:gravel_3")
     public static Block GRAVEL_3;
 
-    @ObjectHolder("compressium:gravel_4")
+    @ObjectHolder("xcompress:gravel_4")
     public static Block GRAVEL_4;
 
-    @ObjectHolder("compressium:gravel_5")
+    @ObjectHolder("xcompress:gravel_5")
     public static Block GRAVEL_5;
 
-    @ObjectHolder("compressium:gravel_6")
+    @ObjectHolder("xcompress:gravel_6")
     public static Block GRAVEL_6;
 
-    @ObjectHolder("compressium:gravel_7")
+    @ObjectHolder("xcompress:gravel_7")
     public static Block GRAVEL_7;
 
-    @ObjectHolder("compressium:gravel_8")
+    @ObjectHolder("xcompress:gravel_8")
     public static Block GRAVEL_8;
 
-    @ObjectHolder("compressium:gravel_9")
+    @ObjectHolder("xcompress:gravel_9")
     public static Block GRAVEL_9;
 
 // Stone
 
-    @ObjectHolder("compressium:stone_1")
+    @ObjectHolder("xcompress:stone_1")
     public static Block STONE_1;
 
-    @ObjectHolder("compressium:stone_2")
+    @ObjectHolder("xcompress:stone_2")
     public static Block STONE_2;
 
-    @ObjectHolder("compressium:stone_3")
+    @ObjectHolder("xcompress:stone_3")
     public static Block STONE_3;
 
-    @ObjectHolder("compressium:stone_4")
+    @ObjectHolder("xcompress:stone_4")
     public static Block STONE_4;
 
-    @ObjectHolder("compressium:stone_5")
+    @ObjectHolder("xcompress:stone_5")
     public static Block STONE_5;
 
-    @ObjectHolder("compressium:stone_6")
+    @ObjectHolder("xcompress:stone_6")
     public static Block STONE_6;
 
-    @ObjectHolder("compressium:stone_7")
+    @ObjectHolder("xcompress:stone_7")
     public static Block STONE_7;
 
-    @ObjectHolder("compressium:stone_8")
+    @ObjectHolder("xcompress:stone_8")
     public static Block STONE_8;
 
-    @ObjectHolder("compressium:stone_9")
+    @ObjectHolder("xcompress:stone_9")
     public static Block STONE_9;
 
 // Iron
 
-    @ObjectHolder("compressium:iron_1")
+    @ObjectHolder("xcompress:iron_1")
     public static Block IRON_1;
 
-    @ObjectHolder("compressium:iron_2")
+    @ObjectHolder("xcompress:iron_2")
     public static Block IRON_2;
 
-    @ObjectHolder("compressium:iron_3")
+    @ObjectHolder("xcompress:iron_3")
     public static Block IRON_3;
 
-    @ObjectHolder("compressium:iron_4")
+    @ObjectHolder("xcompress:iron_4")
     public static Block IRON_4;
 
-    @ObjectHolder("compressium:iron_5")
+    @ObjectHolder("xcompress:iron_5")
     public static Block IRON_5;
 
-    @ObjectHolder("compressium:iron_6")
+    @ObjectHolder("xcompress:iron_6")
     public static Block IRON_6;
 
-    @ObjectHolder("compressium:iron_7")
+    @ObjectHolder("xcompress:iron_7")
     public static Block IRON_7;
 
-    @ObjectHolder("compressium:iron_8")
+    @ObjectHolder("xcompress:iron_8")
     public static Block IRON_8;
 
-    @ObjectHolder("compressium:iron_9")
+    @ObjectHolder("xcompress:iron_9")
     public static Block IRON_9;
 
 // Gold
 
-    @ObjectHolder("compressium:gold_1")
+    @ObjectHolder("xcompress:gold_1")
     public static Block GOLD_1;
 
-    @ObjectHolder("compressium:gold_2")
+    @ObjectHolder("xcompress:gold_2")
     public static Block GOLD_2;
 
-    @ObjectHolder("compressium:gold_3")
+    @ObjectHolder("xcompress:gold_3")
     public static Block GOLD_3;
 
-    @ObjectHolder("compressium:gold_4")
+    @ObjectHolder("xcompress:gold_4")
     public static Block GOLD_4;
 
-    @ObjectHolder("compressium:gold_5")
+    @ObjectHolder("xcompress:gold_5")
     public static Block GOLD_5;
 
-    @ObjectHolder("compressium:gold_6")
+    @ObjectHolder("xcompress:gold_6")
     public static Block GOLD_6;
 
-    @ObjectHolder("compressium:gold_7")
+    @ObjectHolder("xcompress:gold_7")
     public static Block GOLD_7;
 
-    @ObjectHolder("compressium:gold_8")
+    @ObjectHolder("xcompress:gold_8")
     public static Block GOLD_8;
 
-    @ObjectHolder("compressium:gold_9")
+    @ObjectHolder("xcompress:gold_9")
     public static Block GOLD_9;
 
 // Diamond
 
-    @ObjectHolder("compressium:diamond_1")
+    @ObjectHolder("xcompress:diamond_1")
     public static Block DIAMOND_1;
 
-    @ObjectHolder("compressium:diamond_2")
+    @ObjectHolder("xcompress:diamond_2")
     public static Block DIAMOND_2;
 
-    @ObjectHolder("compressium:diamond_3")
+    @ObjectHolder("xcompress:diamond_3")
     public static Block DIAMOND_3;
 
-    @ObjectHolder("compressium:diamond_4")
+    @ObjectHolder("xcompress:diamond_4")
     public static Block DIAMOND_4;
 
-    @ObjectHolder("compressium:diamond_5")
+    @ObjectHolder("xcompress:diamond_5")
     public static Block DIAMOND_5;
 
-    @ObjectHolder("compressium:diamond_6")
+    @ObjectHolder("xcompress:diamond_6")
     public static Block DIAMOND_6;
 
-    @ObjectHolder("compressium:diamond_7")
+    @ObjectHolder("xcompress:diamond_7")
     public static Block DIAMOND_7;
 
-    @ObjectHolder("compressium:diamond_8")
+    @ObjectHolder("xcompress:diamond_8")
     public static Block DIAMOND_8;
 
-    @ObjectHolder("compressium:diamond_9")
+    @ObjectHolder("xcompress:diamond_9")
     public static Block DIAMOND_9;
 
 // Diamond
 
-    @ObjectHolder("compressium:emerald_1")
+    @ObjectHolder("xcompress:emerald_1")
     public static Block EMERALD_1;
 
-    @ObjectHolder("compressium:emerald_2")
+    @ObjectHolder("xcompress:emerald_2")
     public static Block EMERALD_2;
 
-    @ObjectHolder("compressium:emerald_3")
+    @ObjectHolder("xcompress:emerald_3")
     public static Block EMERALD_3;
 
-    @ObjectHolder("compressium:emerald_4")
+    @ObjectHolder("xcompress:emerald_4")
     public static Block EMERALD_4;
 
-    @ObjectHolder("compressium:emerald_5")
+    @ObjectHolder("xcompress:emerald_5")
     public static Block EMERALD_5;
 
-    @ObjectHolder("compressium:emerald_6")
+    @ObjectHolder("xcompress:emerald_6")
     public static Block EMERALD_6;
 
-    @ObjectHolder("compressium:emerald_7")
+    @ObjectHolder("xcompress:emerald_7")
     public static Block EMERALD_7;
 
-    @ObjectHolder("compressium:emerald_8")
+    @ObjectHolder("xcompress:emerald_8")
     public static Block EMERALD_8;
 
-    @ObjectHolder("compressium:emerald_9")
+    @ObjectHolder("xcompress:emerald_9")
     public static Block EMERALD_9;
 
 }
