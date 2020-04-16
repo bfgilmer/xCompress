851d223d2208520944c048dcc1bd1cc10f3d6f84
bfgil <bfgil@Cinnamon>
2020-04-16 10:42:23 -0400

    New compressor item completely integrated

---
+++ src/main/java/me/dinnerbeef/compressium/CompresiumItemGroup.java
@@ -0,0 +1,26 @@
+package me.dinnerbeef.compressium;
+
+import me.dinnerbeef.compressium.blocks.ModBlocks;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+
+public class CompresiumItemGroup extends ItemGroup {
+
+	public CompresiumItemGroup() {
+		this(Compressium.MOD_ID);	
+	}
+	
+	public CompresiumItemGroup(String label) {
+		super(label);
+	}
+
+	public CompresiumItemGroup(int index, String label) {
+		super(index, label);
+	}
+
+	@Override
+	public ItemStack createIcon() {
+		return new ItemStack(ModBlocks.COBBLESTONE_1);
+	}
+
+}
