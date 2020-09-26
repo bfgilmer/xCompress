4d2b9068e25b52b3cfcfd29bb6cbfb2593baff2e
bfgil <bfgil@Cinnamon>
2020-09-26 13:58:24 -0400

    Added clayblokck and various fixes

--- src/main/java/com/bfgilmer/xcompress/blocks/ClayBlock.java
+++ src/main/java/com/bfgilmer/xcompress/blocks/ClayBlock.java
@@ -4,11 +4,76 @@ import com.blakebr0.cucumber.block.BaseBlock;
 
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.IBlockReader;
+import net.minecraft.world.World;
+
 
 public class ClayBlock extends BaseBlock {
 
-    public ClayBlock(Integer number) {
+	private int level;
+    public int getLevel() {
+		return level;
+	}
+
+	private void setLevel(int level) {
+		this.level = level;
+	}
+
+	public ClayBlock(Integer number) {
         super(Material.CLAY, p -> p.sound(SoundType.GROUND)
                 .hardnessAndResistance(0.6f*number.floatValue(), 0.6f*(float)Math.pow(2.0f, number.doubleValue())));
+        setLevel(number);
+    }
+    
+    /**
+     * Block's chance to react to a living entity falling on it.
+     */
+    public void onFallenUpon(World worldIn, BlockPos pos, Entity entityIn, float fallDistance) {
+       if (entityIn.isSuppressingBounce()) {
+          super.onFallenUpon(worldIn, pos, entityIn, fallDistance);
+       } else {
+          entityIn.onLivingFall(fallDistance*getLevel(), 0.0F);
+       }
+
+    }
+
+    /**
+     * Called when an Entity lands on this Block. This method *must* update motionY because the entity will not do that
+     * on its own
+     */
+    public void onLanded(IBlockReader worldIn, Entity entityIn) {
+       if (entityIn.isSuppressingBounce()) {
+          super.onLanded(worldIn, entityIn);
+       } else {
+          this.func_226946_a_(entityIn);
+       }
+
+    }
+
+    private void func_226946_a_(Entity p_226946_1_) {
+       Vec3d vec3d = p_226946_1_.getMotion();
+       if (vec3d.y < 0.0D) {
+          double d0 = p_226946_1_ instanceof LivingEntity ? 1.0D : 0.8D;
+          d0 *= (double) this.getLevel();
+          p_226946_1_.setMotion(vec3d.x, -vec3d.y * d0, vec3d.z);
+       }
+
+    }
+
+    /**
+     * Called when the given entity walks on this Block
+     */
+    public void onEntityWalk(World worldIn, BlockPos pos, Entity entityIn) {
+       double d0 = Math.abs(entityIn.getMotion().y);
+       if (d0 < 0.1D && !entityIn.isSteppingCarefully()) {
+          double d1 = 0.4D + d0 * 0.2D * (double) this.getLevel();
+          entityIn.setMotion(entityIn.getMotion().mul(d1, 1.0D, d1));
+       }
+
+       super.onEntityWalk(worldIn, pos, entityIn);
     }
 }
--- src/main/java/com/bfgilmer/xcompress/blocks/StoneBlock.java
+++ src/main/java/com/bfgilmer/xcompress/blocks/StoneBlock.java
@@ -4,12 +4,37 @@ import com.blakebr0.cucumber.block.BaseBlock;
 
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.entity.Entity;
+import net.minecraft.util.DamageSource;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraft.entity.monster.MonsterEntity;
 
 public class StoneBlock extends BaseBlock {
 
+	private Integer level;
+    public static final DamageSource STONE_DAMAGE = new DamageSource("Compressium");
+	
     public StoneBlock(Integer number) {
-        super(Material.ROCK, SoundType.STONE, 6.0f, 1.5f*number.floatValue());
+        super(Material.ROCK,  p -> p.sound(SoundType.STONE).hardnessAndResistance(2.0f*number.floatValue(), (float) Math.pow(6.0f, number.doubleValue())));
+        setLevel(number);
     }
+
+	@Override
+	public void onEntityWalk(World worldIn, BlockPos pos, Entity entityIn) {
+        if (entityIn instanceof MonsterEntity) {
+	        entityIn.attackEntityFrom(STONE_DAMAGE, 2*level*level);
+        }
+		super.onEntityWalk(worldIn, pos, entityIn);
+	}
+
+	public Integer getLevel() {
+		return level;
+	}
+
+	private void setLevel(Integer level) {
+		this.level = level;
+	}
 }
 
 
