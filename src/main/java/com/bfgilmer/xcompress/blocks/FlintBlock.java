85921fbf2e36bf1224e8e225b0d50b0bcab9c319
Brian F Gilmer <bgilmer@cray.com>
2021-03-11 13:22:33 -0500

    Fixed slime block and flint block damage
    Fixed the issue with the slime blocks not showing up as an item.
    Added damage to entity to match damage of stone blocks.

--- src/main/java/com/bfgilmer/xcompress/blocks/FlintBlock.java
+++ src/main/java/com/bfgilmer/xcompress/blocks/FlintBlock.java
@@ -2,8 +2,15 @@ package com.bfgilmer.xcompress.blocks;
 
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.monster.MonsterEntity;
+import net.minecraft.util.DamageSource;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 
 public class FlintBlock extends FlintHopperBlock {
+    public static final DamageSource FLINT_DAMAGE = new DamageSource("Compressium");
+
     public FlintBlock(Integer number) {
 /*    	super(Material.ROCK, SoundType.GROUND, 6.0f*number.floatValue(), (float)Math.pow(5.0f,number.doubleValue())); */
         super(Properties.create(Material.ROCK)
@@ -11,4 +18,14 @@ public class FlintBlock extends FlintHopperBlock {
                 .hardnessAndResistance(0.6f*number.floatValue(), 0.6f*(float)Math.pow(2.0f, number.doubleValue())));      
         setRank(number);
     }  
+    
+	@Override
+	public void onEntityWalk(World worldIn, BlockPos pos, Entity entityIn) {
+		Integer level = getRank();
+        if (entityIn instanceof MonsterEntity) {
+	        entityIn.attackEntityFrom(FLINT_DAMAGE, 2*level*level);
+        }
+		super.onEntityWalk(worldIn, pos, entityIn);
+	}
+
 }
