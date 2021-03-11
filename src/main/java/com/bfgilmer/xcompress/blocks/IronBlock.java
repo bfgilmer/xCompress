c4575447bbd5877498a2635d45b82e432a0dd039
bfgil <bfgil@Cardamon.fios-router.home>
2021-03-11 09:05:15 -0500

    Fixed Slime blocks and added damage to flint blocks

--- src/main/java/com/bfgilmer/xcompress/blocks/IronBlock.java
+++ src/main/java/com/bfgilmer/xcompress/blocks/IronBlock.java
@@ -3,11 +3,8 @@ package com.bfgilmer.xcompress.blocks;
 
 import com.blakebr0.cucumber.block.BaseBlock;
 
-import net.minecraft.block.BlockState;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.IWorldReader;
 
 public class IronBlock extends BaseBlock {
 
